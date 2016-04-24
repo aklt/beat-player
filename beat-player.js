df0a09e8eca612e9b4d19b9ee3f85288bf654e80
Anders Thøgersen <anders@bladre.dk>
2016-04-24 13:05:03 +0200

    lint

--- beat-player.js
+++ beat-player.js
@@ -1,20 +1,20 @@
-/*global ab, Audio*/
+/*global ab, Audio, requestAnimationFrame*/
 (function (win) {
   var bp = win.beatPlayer = {}
 
   ab.classes = {}
 
   var keyboardKeys = [
-        '1234567890',
-        'QWERTYUIOP',
-        'ASDFGHJKL',
-        'ZXCVBNM,.']
+    '1234567890',
+    'QWERTYUIOP',
+    'ASDFGHJKL',
+    'ZXCVBNM,.']
 
   // {{{1 KeyboardView
   function KeyboardView (o) {
     o = o || {}
     this.ranges = o.ranges || {
-      // Assumption: The array is sorted as the keyboard 
+      // Assumption: The array is sorted as the keyboard
       // and intervals do not overlap
       1: ['U', 'F'],
       2: ['Q', 'R']
@@ -34,7 +34,6 @@
       return result
     },
     template: function (o) {
-      var self = this
       var rangeStart = this.rangesToIndex()
       var rangeEnd = {}
       return ab.templates.keyboard(keyboardKeys.map((row, j) => {
@@ -42,7 +41,7 @@
         var chars = row.split('')
         if (j === 0) return ab.templates.keyboardRow(chars)
         for (var i = 0; i < chars.length; i += 1) {
-          var ch = chars[i];
+          var ch = chars[i]
           if (rangeStart[ch]) {
             rangeEnd = ab.extend(rangeEnd, rangeStart[ch])
             keys += '<span style=\'background-color: ' + ab.color(j + i) + '\'>'
@@ -52,7 +51,7 @@
           if (rangeEnd[ch]) {
             keys += '</span>'
             delete rangeEnd[ch]
-          } 
+          }
         }
         return (j > 1 ? ' ' : '') + keys
       }))
@@ -101,9 +100,7 @@
 
   // 1}}} KeyboardView
 
-
   // {{{1 KeyboardHandler
-
   var key0 = '0'.charCodeAt(0)
   var key9 = '9'.charCodeAt(0)
   var keyUp = 38
@@ -111,12 +108,12 @@
   var keyLeft = 37
   var keyRight = 39
   var keyKeys = {}
-  
+
   keyboardKeys.join('').split('').forEach(function (k) {
     keyKeys[k] = 1
   })
 
-  function KeyboardHandler(o) {
+  function KeyboardHandler (o) {
     console.warn('KeyboardHandler', o)
     if (!o.keyboardView) throw new Error('Need o.keyboardView')
     this.keyboardView = o.keyboardView
@@ -207,11 +204,10 @@
 
   Player.prototype = {
     renderTrack: function (track) {
-      var columns = []
+      // var columns = []
       var i = 0
       var length = this.lpb * this.bar * this.bars
       while (i < length) {
-
         i += 1
       }
     },
@@ -231,7 +227,7 @@
         requestAnimationFrame(function () {
           self.step()
         })
-        }, (this.bpm / 60) * this.bar * this.lpb * 100)
+      }, (this.bpm / 60) * this.bar * this.lpb * 100)
     },
     stop: function () {
       if (this.interval) {
@@ -246,198 +242,179 @@
     }
   }
 
-
   ab.mix.dom(Player)
 
-  const alphaNum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  function decToalphanum (num) {
-    if (num >= alphaNum.length) {
-      console.warn('reset num from ', num)
-      num = alphaNum.length - 1
-      console.warn('reset num to', num)
-    }
-    return alphaNum[num]
-
-  }
-
+  const alphaNum = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  // function decToalphanum (num) {
+    // if (num >= alphaNum.length) {
+      // console.warn('reset num from ', num)
+      // num = alphaNum.length - 1
+      // console.warn('reset num to', num)
+    // }
+    // return alphaNum[num]
+  // }
 
   function alphanumToDec (anum) {
     return alphaNum.indexOf(anum + '')
   }
 
   var player1 = ab.player1 = Player.create({
-      id: 'player1',
-      css: {
-        border: '1px solid red'
-      },
-      template: function (o) {
-        console.warn(player1.templates)
-        var outer = ab.templates.outer(o)
-        // console.warn(outer('Hello'))
-        return outer
+    id: 'player1',
+    css: {
+      border: '1px solid red'
+    },
+    template: function (o) {
+      console.warn(player1.templates)
+      var outer = ab.templates.outer(o)
+      // console.warn(outer('Hello'))
+      return outer
     }})
 
   console.warn('Created player', player1)
-
 
   ab.mix.handlers(Player, {
     click: function (ev, el) {
       console.warn('You clicked', ev, el, el.parentNode)
       switch (el.nodeName) {
-      case 'B':
-        ab.dom1 = ab.dom('<input name="input1" type="text" class="small-input" value="' + el.innerText + '"></input>', {
-          css: {
-            border: '1px solid green',
-            'background-color': '#7899AA',
-            width: '1.1rem',
-            height: '1.1rem',
-            'margin-left': '-0.4rem'
-          }
-        })
-        ab.dom1parent = el
-        ab.append(el, ab.dom1)
-        ab.dom1.focus()
-        ab.dom1.select()
-        break;
-      case 'I': // Click top row to go to position
-        console.warn('I', alphanumToDec(el.innerText))
-        this.gotoPos(alphanumToDec(el.innerText))
-        break;
-      case 'P':
-        console.warn('P Instrument', el)
-        var dom2 = ab.dom('<div class="instruments">${instruments}</div>')
-        break;
+        case 'B':
+          ab.dom1 = ab.dom('<input name="input1" type="text" class="small-input" value="' + el.innerText + '"></input>', {
+            css: {
+              border: '1px solid green',
+              'background-color': '#7899AA',
+              width: '1.1rem',
+              height: '1.1rem',
+              'margin-left': '-0.4rem'
+            }
+          })
+          ab.dom1parent = el
+          ab.append(el, ab.dom1)
+          ab.dom1.focus()
+          ab.dom1.select()
+          break
+        case 'I': // Click top row to go to position
+          console.warn('I', alphanumToDec(el.innerText))
+          this.gotoPos(alphanumToDec(el.innerText))
+          break
+        case 'P':
+          console.warn('P Instrument', el)
+          // var dom2 = ab.dom('<div class="instruments">${instruments}</div>')
+          break
       }
     }
   })
 
   // 1}}} Player
 
+  // {{{1 Slider Input
+  function SliderInput (o) {
+    o = o || {}
+  }
 
-// {{{1 Slider Input
-function SliderInput (o) {
-  o = o || {}
-}
+  SliderInput.prototype = {
+    setRange: function (start, stop, value) {
+      value = value || (start + stop) / 2
+      if (!this.sliderEl) this.sliderEl = ab.qs('input[type=range]', this.el)
+      if (!this.textEl) this.textEl = ab.qs('input[type=text]', this.el)
+      ab.attr(this.sliderEl, {min: start,
+                              max: stop,
+                              value: value})
+      ab.attr(this.textEl, {value: value})
+    },
+    setPosition: function (top, left) {
+      ab.css(this.el, {
+        position: 'absolute',
+        top: top + 'px',
+        left: left + 'px'
+      })
+    }
+  }
 
-SliderInput.prototype = {
-  setRange: function (start, stop, value) {
-    value = value || (start + stop) / 2
-    if (!this.sliderEl) this.sliderEl = ab.qs('input[type=range]', this.el)
-    if (!this.textEl) this.textEl = ab.qs('input[type=text]', this.el)
-    ab.attr(this.sliderEl, {min: start,
-                            max: stop,
-                            value: value})
-    ab.attr(this.textEl, { value: value})
-  },
-  setPosition: function (top, left) {
-    ab.css(this.el, {
-      position: 'absolute',
-      top: top + 'px',
-      left: left + 'px'
+  ab.mix.dom(SliderInput)
+
+  ab.mix.handlers(SliderInput, {
+    input: function (ev, el) {
+      console.warn('input', el.value)
+    },
+    change: function (ev, el) {
+      console.warn('change', el.value)
+    }
+  })
+  // 1}}} Slider Input
+
+  // {{{1 Slider
+
+  function Slider () {
+
+  }
+
+  ab.mix.dom(Slider)
+
+  Slider.prototype = {
+
+  }
+
+  ab.mix.handlers(Slider, {
+    scroll: function () {
+      console.warn('scroll')
+    }
+  })
+
+  // 1}}} Slider
+
+  // {{{1 Samples
+  function Samples () {
+  }
+
+  ab.mix.dom(Samples)
+
+  Samples.prototype = {
+    draw: function (el) {
+      ab.templates.keyboard({
+      })
+    }
+  }
+
+  // 1}}} Samples
+
+  ab.ready(function () {
+    console.warn('ready', bp.sounds.bd)
+    console.warn('ready', bp.sounds.bd.play())
+    // var start = Date.now()
+
+    // KeyboardView
+    var kv1 = KeyboardView.create()
+    kv1.render()
+    kv1.attach('#keyboard')
+
+    // KeyboardHandler handles events on body
+    var kb1 = new KeyboardHandler({
+      keyboardView: kv1
     })
-  }
-}
+    kb1.eventsAttach()
 
-ab.mix.dom(SliderInput)
+    ab.kv1 = kv1
 
-ab.mix.handlers(SliderInput, {
-  input: function (ev, el) {
-    console.warn('input', el.value)
-  },
-  change: function (ev, el) {
-    console.warn('change', el.value)
-  }
-})
-// 1}}} Slider Input
+    var si1 = SliderInput.create({el: ab.qs('#slider1')})
+    si1.setRange(0, 100)
+    si1.setPosition(100, 100)
+    si1.eventsAttach()
 
-bp.el = ab.qs('#player1')
-if (!bp.el) throw new Error('BAd')
-player1.render('Hello')
-
-
-var state = 1
-function stateHandler (nextKey) {
-  switch (nextKey) {
-  }
-}
-
-// {{{1 Slider
-
-function Slider () {
-
-}
-
-ab.mix.dom(Slider)
-
-Slider.prototype = {
-
-}
-
-ab.mix.handlers(Slider, {
-  scroll: function () {
-    console.warn('scroll')
-  }
-})
-
-// 1}}} Slider
-
-// {{{1 Samples
-function Samples () {
-}
-
-ab.mix.dom(Samples)
-
-Samples.prototype = {
-  draw: function (el) {
-    ab.templates.keyboard({
-    })
-  }
-}
-
-// 1}}} Samples
-
-ab.ready(function () {
-  console.warn('ready', bp.sounds.bd);
-  console.warn('ready', bp.sounds.bd.play());
-  var start = Date.now()
-
-
-  // KeyboardView
-  var kv1 = KeyboardView.create()
-  kv1.render()
-  kv1.attach('#keyboard')
-
-  // KeyboardHandler handles events on body
-  var kb1 = new KeyboardHandler({
-    keyboardView: kv1
-  })
-  kb1.eventsAttach()
-
-  ab.kv1 = kv1
-
-  var si1 = SliderInput.create({el: ab.qs('#slider1')})
-  si1.setRange(0, 100)
-  si1.setPosition(100, 100)
-  si1.eventsAttach()
-
-  player1.attach(bp.el)
-  player1.start()
-  ab.delay(1001, () => {
-    console.warn('syop')
-    player1.stop()
-  })
-  ab.delay(2001, () => {
-    console.warn('set')
-    player1.gotoPos(4)
-  })
-  ab.delay(2501, () => {
-    console.warn('Hello')
+    player1.attach(bp.el)
     player1.start()
+    ab.delay(1001, () => {
+      console.warn('syop')
+      player1.stop()
+    })
+    ab.delay(2001, () => {
+      console.warn('set')
+      player1.gotoPos(4)
+    })
+    ab.delay(2501, () => {
+      console.warn('Hello')
+      player1.start()
+    })
+
+    var player = new Player({id: 'player0'})
+    player.eventsAttach()
   })
-
-  var player = new Player({id: 'player0'})
-  player.eventsAttach()
-})
-
-
 }(window))
