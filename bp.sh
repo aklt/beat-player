8934b0d475da9e2d73cd8f4240de6b9cf93151f5
Anders Remote <anders@bladre.dk>
2016-06-17 13:48:00 +0200

    rm templates

--- bp.sh
+++ bp.sh
@@ -3,7 +3,7 @@
 cat << "EOHEADER";
 ;(function () {
 EOHEADER
-cat lib.js model.js audio.js view.js templates.js ready.js test.js | \
+cat lib.js model.js audio.js view.js ready.js test.js | \
     sed -e 's/^/  /g'
 cat << "EOFOOTER";
 }())
